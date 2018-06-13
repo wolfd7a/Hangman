916c3dabb9db5ba7c95ef87e8d5d6908e1162c6b
wolfd7a <39986802+wolfd7a@users.noreply.github.com>
2018-06-13 13:28:03 +0200

    V1
    Working algorithm

--- hangman.py
+++ hangman.py
@@ -1,21 +1,36 @@
 def hangman() :
-    wordletters=['d', 'o','o' 'r']
+    word = 'sir'
+    wordletters=list(word)
+    print(wordletters)
     usedletters=[]
     correctletters=[]
-    counter=0
-    while counter<11 :
-        letter = input('Input a letter :')
-        while (wordletters)!=(correctletters) :
+    counter=10
+    while counter>0 :
+        if ''.join(correctletters)==''.join(wordletters):
+            print(str(correctletters))
+            print('You won !')
+            break
+        else :
+            letter = input('Input a letter :')
             try :
-                if letter not in (usedletters) :
+                letter not in (usedletters)
+                if letter in (wordletters):
+                    print('Usedlist before : '+str(usedletters)) #test only
                     usedletters.append(letter)
+                    print('Usedlist after : '+str(usedletters)) #test only
+                    print('Correctlist before : '+str(correctletters)) #test only
                     correctletters.append(letter)
-                    break
+                    print('Correctlist after : '+str(correctletters)) #test only
+                    continue
                 else :
-                    print('This letter has already been chosen. Choose another one.')
+                    print(usedletters)
+                    usedletters.append(letter)
+                    print(usedletters)
+                    print(counter)
+                    counter-=1
+                    print(counter)
+                    print('This letter is not in the word I chose. '+str(counter)+' attempt(s) left')
+                    continue
             except :
-                usedletters.append(letter)
-                print('This letter is not in the word I chose.')
-                counter+=1
-                break
+                print('This letter has already been chosen. Choose another one.')
 hangman()
