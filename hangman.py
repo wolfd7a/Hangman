35a63549a596c6b9f89e0ba931c4cd3ab18718be
wolfd7a <39986802+wolfd7a@users.noreply.github.com>
2018-06-13 13:38:15 +0200

    V1.1
    Win & Loss messages.

--- hangman.py
+++ hangman.py
@@ -7,30 +7,23 @@ def hangman() :
     counter=10
     while counter>0 :
         if ''.join(correctletters)==''.join(wordletters):
-            print(str(correctletters))
-            print('You won !')
+            print('You won ! '+str(word)+' was the correct word.')
             break
         else :
             letter = input('Input a letter :')
             try :
                 letter not in (usedletters)
                 if letter in (wordletters):
-                    print('Usedlist before : '+str(usedletters)) #test only
                     usedletters.append(letter)
-                    print('Usedlist after : '+str(usedletters)) #test only
-                    print('Correctlist before : '+str(correctletters)) #test only
                     correctletters.append(letter)
-                    print('Correctlist after : '+str(correctletters)) #test only
                     continue
                 else :
-                    print(usedletters)
                     usedletters.append(letter)
-                    print(usedletters)
-                    print(counter)
                     counter-=1
-                    print(counter)
                     print('This letter is not in the word I chose. '+str(counter)+' attempt(s) left')
                     continue
             except :
                 print('This letter has already been chosen. Choose another one.')
+    while counter==0 :
+        print("No lives left. You've lost")
 hangman()
